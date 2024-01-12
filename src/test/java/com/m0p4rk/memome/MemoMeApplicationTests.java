5ce4165eadf955f34dcc7b6d07421efc57154942
mose park <mose.park@aol.com>
2024-01-12 16:04:37 +0900

    Project Structure created

---
+++ src/test/java/com/m0p4rk/memome/MemoMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m0p4rk.memome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
